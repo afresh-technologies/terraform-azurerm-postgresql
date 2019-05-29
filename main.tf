65a3ebf5b45f5957c52dd3ee0dca7d26e1dc42e0
Bill Fraser <fraser@pythian.com>
2019-05-29 13:24:56 -0400

    Comment provider block as it conflicts with configuration in parent project, such as service principal

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
-provider "azurerm" {
-  version = ">=1.16.0"
-}
+# provider "azurerm" {
+#   subscription_id = "REPLACE-WITH-YOUR-SUBSCRIPTION-ID"
+#   client_id       = "REPLACE-WITH-YOUR-CLIENT-ID"
+#   client_secret   = "REPLACE-WITH-YOUR-CLIENT-SECRET"
+#   tenant_id       = "REPLACE-WITH-YOUR-TENANT-ID"
+# }
 
 resource "azurerm_postgresql_server" "server" {
   name                = "${var.server_name}"
